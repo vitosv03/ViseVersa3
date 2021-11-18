d2ed234144e5ea52ba2834dd47a4f8165ce7c2e0
vitosv03 <vitosv03@gmail.com>
2021-11-18 20:26:30 +0200

    create main dev

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+# create main dev
+
+
+
+
 # This is a sample Python script.123
-# dev 1233
+#
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
